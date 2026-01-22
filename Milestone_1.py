a120bbfbef068549293b69b6dae41700d09d100b
David Gan <166639546+kingbadger18@users.noreply.github.com>
2026-01-22 02:32:02 +0100

    implemented valid_moves function

--- Milestone_1.py
+++ Milestone_1.py
@@ -1,5 +1,7 @@
 #Reference: https://www.geeksforgeeks.org/dsa/minimum-steps-reach-target-knight/
 
+from collections import deque
+
 class Chess(object):
     
     def __init__(self, size_x, size_y, start_x, start_y, piece):
@@ -9,42 +11,133 @@ class Chess(object):
         self.start_y = start_y
         self.piece = piece 
         
-    def is_board_valid(self):
-        
-        return 0 <= self.start_x < self.size_x and 0 <= self.start_y < self.size_y
-    
     def valid_moves(self):
+        
+        moves = []
         
         #assume pawn only moves one step forward a time
         #disregard the rule when the pawn can move two steps ahead at its first move.
         #disregard pawn promotion in this milestone
         if self.piece == "pawn":
-            self.x_moves = [0]
-            self.y_moves = [1]
-   
+            
+            moves.append((self.start_x, self.start_y + 1))
+            
+            print(moves)
+        
         elif self.piece == "knight":
-            self.x_moves = [-2, -1, 1, 2, 2, 1, -1, -2]
-            self.y_moves = [1, 2, 2, 1, -1, -2, -2, -1]
+            
+            directions = [
+                (2, 1), 
+                (1, 2), 
+                (-1, 2), 
+                (-2, 1),
+                (-2, -1), 
+                (-1, -2), 
+                (1, -2), 
+                (2, -1)
+            ]
+
+            for dx, dy in directions:
+                
+                x = self.start_x + dx
+                y = self.start_y + dy
+                
+                if 0 <= x < self.size_x and 0 <= y < self.size_y:
+                    moves.append((x,y))
+            
+            print(moves)
             
         elif self.piece == "bishop":
-            self.x_moves = [0]
-            self.y_moves = [1]
+
+            directions = [
+                (1, 1), #up-right
+                (-1, 1), #up-left
+                (-1, -1), #down-right
+                (1, -1) #down-left
+            ]
+            
+            for dx, dy in directions:
+                x = self.start_x + dx
+                y = self.start_y + dy
+                
+                while 0 <= x < self.size_x and 0 <= y < self.size_y:
+                    moves.append((x, y))
+                    x += dx
+                    y += dy
+            
+            print(moves)
 
         elif self.piece == "rook":
-            self.x_moves = [0]
-            self.y_moves = [1]
+            
+            directions = [
+                (1, 0), #right
+                (0, 1), #up
+                (-1, 0), #left
+                (0, -1) #down 
+            ]
+            
+            for dx, dy in directions:
+                x = self.start_x + dx
+                y = self.start_y + dy
+                
+                while 0 <= x < self.size_x and 0 <= y < self.size_y:
+                    moves.append((x, y))
+                    x += dx
+                    y += dy
+
+            print(moves)
 
         elif self.piece == "queen":
-            self.x_moves = [0]
-            self.y_moves = [1]
+            
+            directions = [
+                (1, 1), #up-right
+                (1, 0), #right
+                (-1, -1), #down-right
+                (0, -1) #down
+                (1, -1), #down-left
+                (-1, 0), #left
+                (-1, 1), #up-left
+                (0, 1), #up
+                 
+            ]
+            
+            for dx, dy in directions:
+                x = self.start_x + dx
+                y = self.start_y + dy
+                
+                while 0 <= x < self.size_x and 0 <= y < self.size_y:
+                    moves.append((x, y))
+                    x += dx
+                    y += dy
+            
+            print(moves)            
             
         elif self.piece == "king":
-            self.x_moves = [0]
-            self.y_moves = [1]     
+            
+            directions = [
+                (1, 1), #up-right
+                (1, 0), #right
+                (-1, -1), #down-right
+                (0, -1) #down
+                (1, -1), #down-left
+                (-1, 0), #left
+                (-1, 1), #up-left
+                (0, 1), #up 
+            ]
+            
+            for dx, dy in directions:
+                
+                x = self.start_x + dx
+                y = self.start_y + dy
+                
+                if 0 <= x < self.size_x and 0 <= y < self.size_y:
+                    moves.append((x,y))
+            
+            print(moves)     
 
     def BFS_shortest_path(self):
-        
-        return 0 
+
+        return 0
 
     def print_board(self):
         
@@ -56,8 +149,14 @@ class Chess(object):
     
 if __name__ == "__main__":
     
-    size_x = int(input("Enter the horizontal size of the chess board:").strip())
-    size_y = int(input("Enter the vertical size of the chess board:").strip())
-    start_x = int(input("Entrer the x_coordinate of the starting position:").strip())
-    start_y = int(input("Entrer the y_coordinate of the starting position:").strip())
-    piece = str(input("Enter the name of the piece:").strip()) 
+    #size_x = int(input("Enter the horizontal size of the chess board:").strip())
+    #size_y = int(input("Enter the vertical size of the chess board:").strip())
+    #start_x = int(input("Entrer the x_coordinate of the starting position:").strip())
+    #start_y = int(input("Entrer the y_coordinate of the starting position:").strip())
+    #piece = str(input("Enter the name of the piece:").strip()) 
+    
+    c = Chess(8, 8, 3, 4, "king")
+    
+    c.valid_moves()
+    
+    
